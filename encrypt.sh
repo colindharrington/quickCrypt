b841e4e3e5ae77c4568eba6685cc471ff75a240a
colindharrington <colindavidharrington@gmail.com>
2022-05-28 13:22:39 -0700

    reorder encrypt prompt

--- encrypt.sh
+++ encrypt.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 echo ""
 cd files
-echo -e "\033[1;103m\033[1;30m  Give file a name  \033[0m\033[0m"
-echo ""
 echo "Date/time  ----  $(date +%m.%d.%Y-%H.%M)" > .tmp.tmp
 nano .tmp.tmp
+echo -e "\033[1;103m\033[1;30m  Give file a name  \033[0m\033[0m"
 echo ""
 read name
 echo ""
